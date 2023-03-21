4d77f08aaa27fc362a5dc927717ad218de159f9a
Joshpowerfit <nwankwojoshua100@gmail.com>
2023-03-21 05:54:53 -0700

    correction task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""Script to filter states"""
-
-
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
 
-def main():
-    """Main function"""
-
-    username = argv[1]
-    password = argv[2]
-    dbname = argv[3]
-
-    database = MySQLdb.connect(host="localhost",
-                               port=3306,
-                               user=username,
-                               passwd=password,
-                               db=dbname)
-    cur = database.cursor()
-    cur.execute("SELECT * FROM states WHERE name \
-    LIKE BINARY 'N%' ORDER BY id ASC")
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
-    database.close()
-
-if __name__ == '__main__':
-    main()
+    db.close()
--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""Module to list all states that contain an a"""
-
-
-from sys import argv
-from relationship_city import Base, City
-from relationship_state import State
-from sqlalchemy import create_engine
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import relationship
 
 
-if __name__ == '__main__':
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).options(joinedload(State.cities))
-
-    for states in state:
-        print("{}: {}".format(states.id, states.name))
-        for i in range(len(states.cities)):
-            print("\t{}: {}".format(
-                states.cities[i].id, states.cities[i].name))
-
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
